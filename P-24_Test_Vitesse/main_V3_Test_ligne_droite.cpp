488cd61c0bca6318003ddece1e18693000165611
Revixa1 <57970411+Revixa1@users.noreply.github.com>
2022-11-17 11:58:35 -0500

    ok

--- P-24_Test_Vitesse/main_V3_Test_ligne_droite.cpp
+++ P-24_Test_Vitesse/main_V3_Test_ligne_droite.cpp
@@ -21,43 +21,5 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if(digitalRead(LS_Arriere)==HIGH){
-    for(int i=1;i<=nbrintervale;i=i+a){
-    a=0;
-    if(ENCODER_Read(0)<=(distance_parcourir/nbrintervale)){
-      MOTOR_SetSpeed(0,0.5);
-      ok_gauche=0;
-    }
 
-      if(ENCODER_Read(1)<=(distance_parcourir/nbrintervale)){
-      MOTOR_SetSpeed(1,0.5);
-      ok_droit=0;
-      }
-     
-        
-     if(ENCODER_Read(0)>=distance_parcourir/nbrintervale){
-      MOTOR_SetSpeed(0,0);
-      
-      ok_gauche=1;
-    }
-
-    if(ENCODER_Read(1)>=(distance_parcourir/nbrintervale)){
-      MOTOR_SetSpeed(1,0);
-      
-      ok_droit=1;
-    }
-  
-    if(ok_droit==1 & ok_gauche==1){
-    ENCODER_ReadReset(0);
-    ENCODER_ReadReset(1);
-    a=1;
-    }
-    }
-  }
- 
-  MOTOR_SetSpeed(0,0);
-  MOTOR_SetSpeed(1,0);
-  ENCODER_ReadReset(0);
-  ENCODER_ReadReset(1);
 }
